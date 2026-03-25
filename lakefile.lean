6176722744898a8245fe8a8bc3f3d4a5f5458e70
Matthew Ballard <matt@mrb.email>
2026-03-25 12:12:36 -0400

    chore: pin batteries and Cli to main

--- lakefile.lean
+++ lakefile.lean
@@ -21,8 +21,11 @@ lean_exe add_position_info where
   root := `scripts.convert.add_position_info
   supportInterpreter := true
 
+require batteries from git
+  "https://github.com/leanprover-community/batteries" @ "main"
+
 require Cli from git
-  "https://github.com/mhuisi/lean4-cli" @ "v4.29.0-rc6"
+  "https://github.com/leanprover/lean4-cli" @ "main"
 
 def buildModuleBlueprint (mod : Module) (ext : String) (extractArgs : Array String) : FetchM (Job Unit) := do
   let exeJob ← extract_blueprint.fetch
